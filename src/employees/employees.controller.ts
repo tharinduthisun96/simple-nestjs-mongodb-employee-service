4bf85de46744d4740326b4c83f9b0c08dafc85eb
Tharindu Akalaka <tharindu.thisun123@gmail.com>
2021-08-19 16:21:34 +0530

    employee service making

---
+++ src/employees/employees.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { EmployeesService } from './employees.service';
+
+@Controller('employees')
+export class EmployeesController {
+
+    constructor(private employeeService:EmployeesService){}
+
+    @Get()
+    getAllEmployees(){
+        return this.employeeService.getAllEmployees();
+    }
+}
